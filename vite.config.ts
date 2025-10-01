23ed55217606f78b838627b264217f7e7c4d7f23
Ronald Rivadeneyra <devsoftware993@gmail.com>
2025-10-01 02:36:07 -0500

    Preparando deploy a GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ⚡️ Cambia "Delivery_TiempoReal_V2" por el nombre EXACTO de tu repo en GitHub
 export default defineConfig({
   plugins: [react()],
   base: process.env.GITHUB_PAGES
-    ? '/Delivery_TiempoReal_V2/'
-    : '/',
+    ? "/Delivery_TiempoReal_V2/"
+    : "/",
 })
