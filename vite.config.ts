ca3dd0e61e48a5dd1c6a63d4f7bfd588911f3bb0
Ronald Rivadeneyra <devsoftware993@gmail.com>
2025-10-01 00:04:01 -0500

    chore: dynamic vite base config for Vercel and GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: process.env.VERCEL ? '/' : '/Delivery_TiempoReal_V2/', 
 })
